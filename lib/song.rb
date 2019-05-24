be92b1266cf9f448af925400037a49b4b457d82e
Iskra B <ibatistapoblete@gmail.com>
2019-05-24 05:47:04 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,7 +17,9 @@ class Song
     artist_name = filename.split(" - ")[0]
     song_name = filename.split(" - ")[1]
     song = self.new(song_name)
-    song.artist = Artist.find_or_create_by_name(artist_name)
+    artist = Artist.find_or_create_by_name(artist_name)
+    song.artist = artist
+    artist.add_song(song)
     song
 
   end
