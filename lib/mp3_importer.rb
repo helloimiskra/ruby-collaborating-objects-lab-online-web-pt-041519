3d3b10e954dfbbaa518ff8ba12025404ba39fecd
Iskra B <ibatistapoblete@gmail.com>
2019-05-24 03:26:36 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,7 +1,15 @@
 class MP3Importer
-  attr_accessor :path
+  attr_accessor :path, :files
 
   def initialize(path)
     @path = path
   end
+
+  def files
+    @files = Dir.glob("*.MP3")
+  end
+
+  def import
+  end
+
 end
