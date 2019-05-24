fdfa3f6d52ee8dfedf60a8c9a0ee657b287ec0d3
Iskra B <ibatistapoblete@gmail.com>
2019-05-24 03:57:47 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,9 +3,13 @@ class Song
 
   def initialize(name)
     @name = name
+    @artist = artist
   end
 
   def self.new_by_filename(filename)
+    file_name = filename.split(' - ')
+    song.name = file_name[0]
+    song.artist =  file_name[1]
   end
 
 
