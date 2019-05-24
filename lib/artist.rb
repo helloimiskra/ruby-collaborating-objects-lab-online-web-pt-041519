1f1bade22e8af1951cd4e3327f8a0af1c5dbd864
Iskra B <ibatistapoblete@gmail.com>
2019-05-24 03:04:13 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,13 +1,21 @@
 class Artist
-  attr_accessor :name
+  attr_accessor :name, :artist
   attr_reader :songs
+
+  @@all = []
 
   def initialize(name)
     @name = name
     @songs = []
+    @artist = artist
   end
 
   def add_song(song)
     @songs << song
   end
+
+  def save
+    @@all << song.artist 
+  end
+    
 end
